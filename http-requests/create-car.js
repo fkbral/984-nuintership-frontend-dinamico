1bd17dfbb09b9455c86ac4807740110b4387e6cc
Felipe Cabral <felipeoc5@hotmail.com>
2023-04-13 14:31:58 -0300

    feat: car create exemple start

---
+++ http-requests/create-car.js
@@ -0,0 +1,24 @@
+import { v4 as uuid } from "uuid";
+import { baseUrl } from "./main";
+
+const carToCreateBody = {
+  id: uuid(),
+  name: "Ford Fiesta",
+  color: "grey",
+  amountInStock: 20,
+};
+
+const createCarForm = document.querySelector("[data-create-car]");
+
+// Salve o carro no carro e mostre-o na listagem sem ter que recarregar a página
+createCarForm.onsubmit = (event) => {
+  event.preventDefault();
+};
+
+// fetch(`${baseUrl}/cars`, {
+//   method: "POST",
+//   headers: {
+//     "Content-type": "application/JSON",
+//   },
+//   body: JSON.stringify(carToCreateBody),
+// });
